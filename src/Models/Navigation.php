af034f088b1c5ffd04624d466c45cf00a0a7a3fc
Eduardo Carvalho <eduardo@code49.net>
2023-09-18 16:44:00 -0300

    🐛 Fix

--- src/Models/Navigation.php
+++ src/Models/Navigation.php
@@ -2,9 +2,10 @@
 
 namespace duscarvalho\FilamentNavigation\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use App\Models\Company;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * @property string $handle
